d8d306691522d0572dae9ba39f670315d2bd7753
Marina-Hizhnyak <maryfadeeva1208@gmail.com>
2025-08-26 21:11:31 +0200

    correction refetchOnMount

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -12,6 +12,7 @@ export default function NoteDetailsClient() {
     queryKey: ['note', id],
     queryFn: () => fetchNoteById(id),
     enabled: typeof id === 'string' && id.length > 0,
+    refetchOnMount: false,
   });
 
   if (isLoading) return <p>Loading, please wait...</p>;
